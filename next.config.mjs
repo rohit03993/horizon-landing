645c92938a7ad2e05e7b5c17d41c2a860bc1e0c7
Rohit Pal <rohit03993@gmail.com>
2025-12-27 12:59:28 +0530

    Limit Next.js workers to fix build issues

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ['instagram.com', 'scontent.cdninstagram.com'],
   },
+  // Limit workers to avoid resource issues
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+  },
 };
 
 export default nextConfig;
